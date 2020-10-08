6d056c2af163018d30554ff6797bf604ba19cff8
Apoorv <agrawal.av27@gmail.com>
2020-10-08 20:48:08 +0530

    Updates

---
+++ precache-manifest.0ee1d18a5eda05bcdec9e08d511cf3e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588657cd5e9b7057abd86d49feec887b",
+    "url": "/spacx-project/index.html"
+  },
+  {
+    "revision": "0c3e9851d926d831799a",
+    "url": "/spacx-project/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7010c4af167f403aa957",
+    "url": "/spacx-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0c3e9851d926d831799a",
+    "url": "/spacx-project/static/js/2.b156c24e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/spacx-project/static/js/2.b156c24e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7010c4af167f403aa957",
+    "url": "/spacx-project/static/js/main.38a886fa.chunk.js"
+  },
+  {
+    "revision": "73b843b2f7a968bc1026",
+    "url": "/spacx-project/static/js/runtime-main.4e56c602.js"
+  }
+]);
--- src/components/childComponents/HeaderComponent.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-class HeaderComponent extends Component {
-  state = {};
-  render() {
-    return <h3>SpaceX Launch Programs</h3>;
-  }
-}
-
-export default HeaderComponent;
